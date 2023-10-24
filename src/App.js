5deb51ecfdb43f68df88a7c61795a869316f100e
Psam4ord <psam4ord@gmail.com>
2023-10-24 10:14:29 +0000

    Tailwindcss configured

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@
 function App() {
   return (
     <div className="App">
-    <h1>Hello World</h1>
+    <h1 class = "bg-black text-white flex flex-col m-3" >Hello World</h1>
+    <button class="bg-cyan-500 hover:bg-cyan-600 border-spacing-1 flex  ">  Subscribe </button>
     </div>
   );
 }
