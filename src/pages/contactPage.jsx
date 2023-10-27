7b459c9c319e63e0b98ce301acc418309eaa0c69
Psam4ord <psam4ord@gmail.com>
2023-10-27 15:56:26 +0000

    building about page

--- src/pages/contactPage.jsx
+++ src/pages/contactPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import NavBar from '../Components/Layout/navBar';
+import Footer from '../Components/Layout/footer';
 
 const Contact = () => {
   return (
     <div> 
+      <NavBar />
         <h1> Contact Page</h1>
+        <Footer />
     </div>
   )
 }
--- src/pages/servicePage.jsx
+++ src/pages/servicePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import NavBar from '../Components/Layout/navBar';
+import Footer from '../Components/Layout/footer';
 
 const ServicePage = () => {
   return (
     <div> 
+      <NavBar/>
         <h1>Services Page</h1>
+        <Footer />
     </div>
   )
 }
