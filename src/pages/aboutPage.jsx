be77193d5924a17efe18b2aa4f85c2f41ef0c9dd
psam4ord <psam4ord@gmail.com>
2023-11-16 01:17:46 +0000

    fixes: touches

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -87,19 +87,7 @@ const AboutPage = () => {
 
       </div>
 
-      <div className=' bg-bgPrimary rounded-md p-3 m-2'>
-        <h3 className='uppercase text-center p-1 font-bold'> Portfolio</h3>
-        <ul className='flex justify-center xsm:flex-wrap space-x-2 h-auto'>
-         
-        </ul>
-      </div>
-
-      <div className=' bg-bgPrimary rounded-md p-3 m-2'>
-        <h3 className='uppercase text-center p-1 font-bold'> Testimonials</h3>
-        <ul className='flex justify-center xsm:flex-wrap space-x-2 h-auto'>
-         
-        </ul>
-      </div>
+    
 
       <Footer />
     </>
--- src/pages/servicePage.jsx
+++ src/pages/servicePage.jsx
@@ -12,20 +12,31 @@ const ServicePage = () => {
       <NavBar />
       <div className=' mt-20 p-4'>
         {/* Portfolio works here*/}
-        <h1 className=' p-1 uppercase font-medium'>Portfolio</h1>
 
-        {/* image sample here */}
-        <div className='xsm:w-full  border-bgSecondary '>
-          <li className='hover:text-[#78dcfa]'><a href='https://frontendmentor-landingpage.netlify.app'
-            target="_blank" rel="noreferrer"> Mentorship Program </a> </li>
-        </div>
 
-        <div>
-          <li className='hover:text-[#78dcfa]'><a href='https://kerry-gallery.netlify.app/'
-            target="_blank" rel="noreferrer"> Gallery clone </a> </li>
+        <div className=' bg-bgPrimary rounded-md p-3 m-2'>
+          <h3 className='uppercase text-center p-1 font-bold'> Portfolio</h3>
+          <ul className='flex justify-center xsm:flex-wrap space-x-2 h-auto'>
+          </ul>
+          {/* image sample here */}
+          <div className='xsm:w-full  border-bgSecondary '>
+            <li className='hover:text-[#78dcfa]'><a href='https://frontendmentor-landingpage.netlify.app'
+              target="_blank" rel="noreferrer"> Mentorship Program </a> </li>
+          </div>
+
+          <div>
+            <li className='hover:text-[#78dcfa]'><a href='https://kerry-gallery.netlify.app/'
+              target="_blank" rel="noreferrer"> Gallery clone </a> </li>
+          </div>
         </div>
 
         {/* Testimony swipers here */}
+        <div className=' bg-bgPrimary rounded-md p-3 m-2'>
+          <h3 className='uppercase text-center p-1 font-bold'> Testimonials</h3>
+          <ul className='flex justify-center xsm:flex-wrap space-x-2 h-auto'>
+          <p>incoming...</p>
+          </ul>
+        </div>
       </div>
       <Footer />
     </>
